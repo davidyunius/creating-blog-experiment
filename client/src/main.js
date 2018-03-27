b67a35a883722929922827de9d1432a2df805bbd
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-03-27 17:28:41 +0700

    #34 custom directives

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,27 @@ import VueResource from 'vue-resource'
 Vue.config.productionTip = false
 Vue.use(VueResource)
 
+// Custom directives
+Vue.directive('rainbow', {
+  bind (el, binding, vnode) {
+    el.style.color = '#' + Math.random().toString().slice(2, 8)
+  }
+})
+
+Vue.directive('theme', {
+  bind (el, binding, vnode) {
+    if (binding.value === 'wide') {
+      el.style.maxWidth = '1200px'
+    } else if (binding.value === 'narrow') {
+      el.style.maxWidth = '560px'
+    }
+    if (binding.arg === 'column') {
+      el.style.background = '#ddd'
+      el.style.padding = '20px'
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
