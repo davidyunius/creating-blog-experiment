d27ca168e5505e7565199eeae7442e6e73fe1a6d
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-03-27 17:35:57 +0700

    #35 Filter

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,15 @@ import VueResource from 'vue-resource'
 Vue.config.productionTip = false
 Vue.use(VueResource)
 
+// Filters
+Vue.filter('to-uppercase', function (value) {
+  return value.toUpperCase()
+})
+
+Vue.filter('snippet', function (value) {
+  return value.slice(0, 100) + '...'
+})
+
 // Custom directives
 Vue.directive('rainbow', {
   bind (el, binding, vnode) {
